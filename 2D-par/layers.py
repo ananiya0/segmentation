0064383a90d31c271ef7fb9265f30c0ac5dae5ef
sarahkman <sarahk21@vt.edu>
2020-11-13 13:14:45 -0500

    fix layers code

--- 2D-par/layers.py
+++ 2D-par/layers.py
@@ -1,60 +1,40 @@
 import torch
 
-from distdl.utilities.torch import NoneTensor
+import distdl
+from distdl.utilities.torch import zero_volume_tensor
 
 
 class DistributedNetworkOutputFunction(torch.autograd.Function):
 
-	@staticmethod
-	def forward(ctx, input, partition):
-		
-		ctx.partition = partition
+    @staticmethod
+    def forward(ctx, input, partition):
 
-		if partition.rank == 0:
-			return input.clone()
+        ctx.partition = partition
+        ctx.sh = input.shape
 
-		else:
-			return torch.tensor([0.0], requires_grad=True).float()
+        if partition.rank == 0:
+            return input.clone()
+        else:
+            return torch.tensor([0.0], requires_grad=True).float()
 
-	@staticmethod
-	def backward(ctx, grad_output):
+    @staticmethod
+    def backward(ctx, grad_output):
 
-		partition = ctx.partition
+        partition = ctx.partition
+        sh = ctx.sh
 
-		if partition.rank == 0:
-			return grad_output.clone(), None
+        if partition.rank == 0:
+            return grad_output.clone(), None
+        else:
+            return zero_volume_tensor(sh[0]), None
 
-		else:
-			return NoneTensor(), None
 
-class DistributedNetworkOutput(torch.nn.Module):
+class DistributedNetworkOutput(distdl.nn.Module):
 
-	def __init__(self, partition):
-		super(DistributedNetworkOutput, self).__init__()
-		self.partition = partition
+    def __init__(self, partition):
+        super(DistributedNetworkOutput, self).__init__()
+        self.partition = partition
 
-	def forward(self, input):
-		return DistributedNetworkOutputFunction.apply(input, self.partition)
+    def forward(self, input):
 
-# Set of layers implemented in Unet that occurs frequently
-def dist_dual_conv(P_conv, in_channel, out_channel):
-	conv = torch.nn.Sequential(
-		distdl.nn.DistributedConv2d(P_conv,
-					    in_channels = in_channel,
-					    out_channels = out_channel,
-					    kernel_size=(3,3),
-					    padding=(1,1)),
-		# REPLACE W UPSCALING LATER
-		distdl.nn.DistributedBatchNorm(P_conv, num_features=1),
-		torch.nn.ReLU(),
-		distdl.nn.DistributedConv2d(P_conv,
-                                            in_channels = in_channel,
-                                            out_channels = out_channel,
-                                            kernel_size=(3,3),
-                                            padding=(1,1)),
-		# REPLACE W UPSCALING LATER
-		distdl.nn.DistributedBatchNorm(P_conv, num_features=1),
-		torch.nn.ReLU()
-	)
-
-	return conv
+        return DistributedNetworkOutputFunction.apply(input, self.partition)
